88caf31a42a8926df5d229be93b0a1b8e87a7816
Malcolm Mathew <malcolmmmathew@gmail.com>
2016-05-02 12:55:33 -0400

    Added newuser route and functionality.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lyrica' });
 });
 
 router.get('/userlist', function(req, res) {
@@ -14,6 +14,31 @@ router.get('/userlist', function(req, res) {
             "userlist" : docs
         });
     });
+});
+
+router.get('/newuser', function(req, res) {
+	res.render('newuser', { title: 'Add New User' });
+});
+
+router.post('/adduser', function(req, res) {
+	var db = req.db;
+
+	var username = req.body.username;
+	var useremail = req.body.useremail;
+
+	var collection = db.get('usercollection');
+
+	collection.insert({
+		"username": username,
+		"email": useremail
+	}, function (err, doc) {
+		if (err) {
+			res.send("Error adding user.");
+		}
+		else {
+			res.redirect("userlist");
+		}
+	});
 });
 
 module.exports = router;
