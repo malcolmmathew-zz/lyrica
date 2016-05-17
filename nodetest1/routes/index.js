9dbb96c968c60340870ea83771a59643dc34c537
Malcolm Mathew <malcolmmmathew@gmail.com>
2016-05-16 21:48:08 -0400

    Changed modules.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -6,39 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Lyrica' });
 });
 
-router.get('/userlist', function(req, res) {
+router.get('/songlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('usercollection');
+    var collection = db.get('songcollection');
     collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
+        res.render('songlist', {
+            "songlist" : docs
         });
     });
 });
 
-router.get('/newuser', function(req, res) {
-	res.render('newuser', { title: 'Add New User' });
-});
+// router.get('/newuser', function(req, res) {
+// 	res.render('newuser', { title: 'Add New User' });
+// });
 
-router.post('/adduser', function(req, res) {
-	var db = req.db;
+// router.post('/adduser', function(req, res) {
+// 	var db = req.db;
 
-	var username = req.body.username;
-	var useremail = req.body.useremail;
+// 	var username = req.body.username;
+// 	var useremail = req.body.useremail;
 
-	var collection = db.get('usercollection');
+// 	var collection = db.get('usercollection');
 
-	collection.insert({
-		"username": username,
-		"email": useremail
-	}, function (err, doc) {
-		if (err) {
-			res.send("Error adding user.");
-		}
-		else {
-			res.redirect("userlist");
-		}
-	});
-});
+// 	collection.insert({
+// 		"username": username,
+// 		"email": useremail
+// 	}, function (err, doc) {
+// 		if (err) {
+// 			res.send("Error adding user.");
+// 		}
+// 		else {
+// 			res.redirect("userlist");
+// 		}
+// 	});
+// });
 
 module.exports = router;
